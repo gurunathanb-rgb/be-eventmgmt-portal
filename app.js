99e970c32838b3ec0f03ae36497456670fe14071
gurunathanb-rgb <gurunathanb@gmail.com>
2026-09-07 15:50:52 +0530

    cors updated

--- app.js
+++ app.js
@@ -15,34 +15,17 @@ const app = express();
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-
-
-
-// 🌟 FIX 2: Dynamic CORS origin processing array configuration rules
-const allowedOrigins = [
-  'http://localhost:5173',
-  'http://127.0.0.1:5173',
-  'http://localhost:3001',
-  'http://127.0.0.1:3001',
-  'https://fe-eventmgmt-portal.netlify.app/api'
-];
-
 // 2. Global System Middlewares
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allows server-to-server or tools like Postman (where origin is undefined)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  //origin: 'http://localhost:5173', // Your Vite frontend location URL string
+   //origin: 'http://127.0.0.1:5173',
+   origin: 'https://fe-eventmgmt-portal.netlify.app',
   credentials: true,                // Enables passing secure HTTP-Only cookies
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+
 
 app.use(express.json());
 // 1. Mount Stripe Webhook BEFORE general express.json middleware
